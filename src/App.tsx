2a277a3c67c336fb53df0c4d62f99dd21e901b6c
Hoong Ern Ng <hoongern@hoongern.com>
2024-07-07 07:16:13 -0700

    Improve touch handling.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useLayoutEffect, useMemo, useRef, useState } from '
 import './App.css';
 import { useGoogleSheetData, SetupGoogleSheet } from './GoogleSheetDataSource';
 import { CSVToArray } from './csv';
+import { useDrag, useGesture } from '@use-gesture/react';
 
 interface Event {
 	title: string;
@@ -182,33 +183,21 @@ function Timeline({
 		[dragging, extent.end, extent.start],
 	);
 
-	const [touchPosition, setTouchPosition] = useState<{ x: number; y: number } | undefined>(
-		undefined,
-	);
-
-	const handleTouchStart = useCallback(() => {
-		setDragging(true);
-	}, []);
-
-	const handleTouchEnd = useCallback(() => {
-		setDragging(false);
-		setTouchPosition(undefined);
-	}, []);
-
-	const handleTouchMove = useCallback(
-		(e: React.TouchEvent) => {
-			if (dragging && touchPosition) {
-				const delta = e.touches[0].clientX - touchPosition.x;
+	const bind = useGesture(
+		{
+			onDrag: (state) => {
+				if (!state.down) {
+					return;
+				}
+				const delta = state.delta[0];
 				const scale =
 					container.current!.offsetWidth / (extent.end.getTime() - extent.start.getTime());
 				const newStart = new Date(extent.start.getTime() - delta / scale);
 				const newEnd = new Date(extent.end.getTime() - delta / scale);
 				setExtent({ start: newStart, end: newEnd });
-				e.preventDefault();
-			}
-			setTouchPosition({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+			},
 		},
-		[dragging, extent.end, extent.start, touchPosition],
+		{ drag: {} },
 	);
 
 	const todayLeft =
@@ -224,12 +213,10 @@ function Timeline({
 			onMouseDown={handleMouseDown}
 			onMouseUp={handleMouseUp}
 			onMouseMove={handleMouseMove}
-			onTouchStart={handleTouchStart}
-			onTouchEnd={handleTouchEnd}
-			onTouchMove={handleTouchMove}
 			style={{
 				cursor: dragging ? 'grabbing' : 'grab',
 			}}
+			{...bind()}
 		>
 			{Array.from({ length: extent.end.getFullYear() - extent.start.getFullYear() + 2 }).map(
 				(_, i) => {
